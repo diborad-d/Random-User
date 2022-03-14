0f4910bd31c4878ef133ff135ce331d741c3a8d3
Dibora <deboradagne@gmail.com>
2022-03-13 21:20:00 -0500

    import and instantiate context api

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,8 +1,8 @@
-// import { constants } from "buffer";
+
 import { createContext } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = UserContext.Provider;
 
-// export default { UserContext };
+
